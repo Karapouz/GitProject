e8912c827ed52ffeb8e30cb2dcae030568eaf134
apprenant59 <apprenant59@lyn-fmu210024.home>
2022-06-15 14:40:08 +0200

    my first commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitProject
+//
+//  Created by apprenant59 on 15/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
